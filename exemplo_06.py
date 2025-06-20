021defe82ce1fa4dde97de8a6b02aa1e3c5895e0
DevLeonir <juniorjoga102011@hotmail.com>
2025-06-20 19:34:39 -0300

    Manipulando lista de idades

---
+++ exemplo_06.py
@@ -0,0 +1,25 @@
+lista_idades = [12, 20, 78, 67]
+
+lista_idades.append(45)
+
+print(lista_idades)
+
+lista_idades.insert(0, 60)
+
+print(lista_idades)
+
+lista_idades.remove(78)
+
+print(lista_idades)
+
+lista_idades[0] = 89
+
+print(lista_idades)
+
+lista_idades = lista_idades[:-1]
+
+print(lista_idades)
+
+lista_idades.sort()
+
+print(lista_idades)
